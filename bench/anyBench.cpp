e36ecab199f5ab6597a143db263882aa47e63013
Valeriy Avanesov <acopich@gmail.com>
2025-10-11 20:57:12 +0200

    Add more benchmarks for type-erasure.

--- bench/anyBench.cpp
+++ bench/anyBench.cpp
@@ -14,10 +14,10 @@ static auto runAny(Args&& ... args) {
     return any_cast<AnyValueType>(b);
 }
 
-template<typename Any>
-static void benchWithInt(benchmark::State& state) {
+template<typename Any, typename ValueType, auto Value>
+static void benchWithValue(benchmark::State& state) {
   for (auto _ : state) {
-    runAny<Any, int>(42);
+    runAny<Any, ValueType>(Value);
     benchmark::ClobberMemory(); 
   }
 }
@@ -65,17 +65,33 @@ static void benchSwapInt(benchmark::State& state) {
 
 constexpr static int kMinTime = 1;
 
+template <typename Any>
+static auto benchWithInt = benchWithValue<Any, int, 42>;
+
 BENCHMARK(benchWithInt<AnyOnePtr<8>>)->MinTime(kMinTime);
 BENCHMARK(benchWithInt<std::any>)->MinTime(kMinTime);
-BENCHMARK(benchCtorInt<AnyOnePtr<8>>)->MinTime(kMinTime);
-BENCHMARK(benchCtorInt<std::any>)->MinTime(kMinTime);
-BENCHMARK(benchGetInt<AnyOnePtr<8>>)->MinTime(kMinTime);
-BENCHMARK(benchGetWithGet)->MinTime(kMinTime);
-BENCHMARK(benchGetInt<std::any>)->MinTime(kMinTime);
-BENCHMARK(benchMoveInt<AnyOnePtr<8>>)->MinTime(kMinTime);
-BENCHMARK(benchMoveInt<std::any>)->MinTime(kMinTime);
-BENCHMARK(benchSwapInt<AnyOnePtr<8>>)->MinTime(kMinTime);
-BENCHMARK(benchSwapInt<std::any>)->MinTime(kMinTime);
+
+template <typename Any>
+static auto benchWithInt64 = benchWithValue<Any, std::uint64_t, 0xDEADBEEF>;
+
+BENCHMARK(benchWithInt64<AnyOnePtr<8>>)->MinTime(kMinTime);
+BENCHMARK(benchWithInt64<std::any>)->MinTime(kMinTime);
+
+template <typename Any>
+static auto benchWithInt128 = benchWithValue<Any, __int128, 0xDEADDEADBEEF>;
+
+BENCHMARK(benchWithInt128<AnyOnePtr<8>>)->MinTime(kMinTime);
+BENCHMARK(benchWithInt128<std::any>)->MinTime(kMinTime);
+
+//BENCHMARK(benchCtorInt<AnyOnePtr<8>>)->MinTime(kMinTime);
+//BENCHMARK(benchCtorInt<std::any>)->MinTime(kMinTime);
+//BENCHMARK(benchGetInt<AnyOnePtr<8>>)->MinTime(kMinTime);
+//BENCHMARK(benchGetWithGet)->MinTime(kMinTime);
+//BENCHMARK(benchGetInt<std::any>)->MinTime(kMinTime);
+//BENCHMARK(benchMoveInt<AnyOnePtr<8>>)->MinTime(kMinTime);
+//BENCHMARK(benchMoveInt<std::any>)->MinTime(kMinTime);
+//BENCHMARK(benchSwapInt<AnyOnePtr<8>>)->MinTime(kMinTime);
+//BENCHMARK(benchSwapInt<std::any>)->MinTime(kMinTime);
 
 BENCHMARK_MAIN();
 
