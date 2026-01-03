ecf53bc019d9ce00576564069b6743b7320c5411
Valeriy Avanesov <acopich@gmail.com>
2026-01-03 20:34:50 +0100

    Tune Expr storage. Again.

--- Expr.h
+++ Expr.h
@@ -15,7 +15,7 @@ namespace detail{
 template <typename R>
 struct ExprBase : woid::InterfaceBuilder
                       ::With<woid::VTableOwnership::DEDICATED>
-                      ::WithStorage<woid::TrivialStorage<16, woid::Copy::DISABLED>>
+                      ::WithStorage<woid::TrivialStorage<8, woid::Copy::DISABLED>>
                       ::Fun<"generate", [](const auto& obj, BFMachine& bfm) -> R { return obj.generate(bfm); }>
                       ::Build {
     using ExprBase<R>::Self::Self;
